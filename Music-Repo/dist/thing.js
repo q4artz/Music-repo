24cd8f4e3d32a8e7fe7f2b857155c824b381e3eb
q4artz <116222352+q4artz@users.noreply.github.com>
2023-01-14 12:48:00 +0800

    Add files via upload

---
+++ Music-Repo/dist/thing.js
@@ -0,0 +1,6 @@
+"use strict";
+const button = document.querySelector('.btn');
+button.onclick("click", () => {
+    console.log("button pressed");
+});
+//# sourceMappingURL=thing.js.map
---
+++ Music-Repo/src/thing.ts
@@ -0,0 +1,7 @@
+const button :any = document.querySelector('.btn');
+
+button.onclick = () => {
+    console.log("hello")
+};
+
+
